443bae6041f281c7ded1f9f295034a5e1979575a
Noa Tulchinski <noat@nanorep.com>
2016-12-08 14:24:12 +0200

    like disable

--- nanowidget/src/main/java/nanorep/nanowidget/Components/DislikeDialog.java
+++ nanowidget/src/main/java/nanorep/nanowidget/Components/DislikeDialog.java
@@ -2,17 +2,22 @@ package nanorep.nanowidget.Components;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.nanorep.nanoclient.RequestParams.NRLikeType;
 
-import nanorep.nanowidget.DataClasse.NRLikeAdapter;
+import java.util.ArrayList;
+
 import nanorep.nanowidget.R;
 
 /**
@@ -81,4 +86,73 @@ public class DislikeDialog extends AlertDialog.Builder {
         alert.show();
 //        show();
     }
+
+    public class NRLikeAdapter extends ArrayAdapter<String> implements View.OnClickListener {
+        private String[] mObjects;
+        private ArrayList<ImageView> bullets;
+        private NRLikeType mSelection = NRLikeType.INCORRECT_ANSWER;
+
+        public NRLikeAdapter(Context context, int resource, String[] objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+        }
+
+        public NRLikeType getSelection() {
+            return mSelection;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final LayoutInflater inflater = (LayoutInflater) getContext()
+                    .getSystemService(
+                            Context.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.dislike_row, null);
+                convertView.setTag(position);
+                convertView.setOnClickListener(this);
+                TextView titleView = (TextView) convertView.findViewById(R.id.dislike);
+                titleView.setText(mObjects[position]);
+                if (bullets == null) {
+                    bullets = new ArrayList<>();
+                }
+                bullets.add((ImageView) convertView.findViewById(R.id.imageView));
+            }
+            return convertView;
+        }
+
+        private int resId(String resName) {
+            return getContext().getResources().getIdentifier(resName, "drawable", getContext().getPackageName());
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            switch ((int)v.getTag()) {
+                case 0:
+                    mSelection = NRLikeType.INCORRECT_ANSWER;
+                    break;
+                case 1:
+                    mSelection = NRLikeType.MISSING_INFORMATION;
+                    break;
+                case 2:
+                    mSelection = NRLikeType.IRRELEVANT;
+                    break;
+            }
+
+            okButton.setEnabled(true);
+            okButton.setBackgroundColor(Color.parseColor("#0aa0ff"));
+
+            for (int i = 0; i < bullets.size(); i++) {
+                int id;
+                if ((int) v.getTag() == i) {
+                    id = resId("bullet_on");
+                } else {
+                    id = resId("bullet_off");
+                }
+                bullets.get(i).setImageResource(id);
+            }
+        }
+    }
+
 }
